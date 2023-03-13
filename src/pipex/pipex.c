d685eeb0f0501ba5cb811aa67efe9a6fa35742cf
Zackary Trottier <ztrottie@c2r4p6.42quebec.com>
2023-03-13 18:34:21 -0400

    asd

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ztrottie <zakytrottier@hotmail.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 11:36:53 by ztrottie          #+#    #+#             */
-/*   Updated: 2023/03/07 13:18:44 by ztrottie         ###   ########.fr       */
+/*   Updated: 2023/03/13 18:34:11 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,4 @@ int	main(int argc, char **argv, char **env)
 	if (argc != 5)
 		return (0);
 	pipex = ft_calloc(1, sizeof(t_pipex));
-	pipex->path = ft_split(env[2] + 5, ':');
 }
